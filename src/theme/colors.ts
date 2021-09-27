31a2d978cb9a3d829a757b264d541f3e2ec054da
holobleed <80241996+holobleed@users.noreply.github.com>
2021-09-27 18:48:24 -0400

    1

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -24,8 +24,8 @@ export const lightColors: Colors = {
   input: "#eeeaf4",
   tertiary: "#eff2f5",
   text: "#262626",
-  textDisabled: "#2b003d",
-  textSubtle: "#2b003d",
+  textDisabled: "#BDC2C4",
+  textSubtle: "#809fba",
   borderColor: "#E9EAEB",
   card: "#FFFFFF",
   gradients: {
